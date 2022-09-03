ecb0ff31c9154340130ab7c9121430fc6dfaecc4
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-09-04 02:21:52 +0900

    :sparkles: inst mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Container, MantineProvider, Text, Title } from "@mantine/core"
 import React from "react"
 
 export const App = () => {
-  return <p>App</p>
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: "dark" }}
+    >
+      <Container>
+        <Title order={1}>App</Title>
+        <Text>This is App</Text>
+      </Container>
+    </MantineProvider>
+  )
 }
